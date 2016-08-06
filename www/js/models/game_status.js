5ef33c6492238bde25f5260bb0421cdb836983e1
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-06 15:24:18 +0900

    add game status model

---
+++ www/js/models/game_status.js
@@ -0,0 +1,9 @@
+(function () {
+    alcoholicmine.models.GameStatus = {
+        INITIALIZING: 'INITIALIZING',
+        TURN_GAME_MASTER: 'TURN_GAME_MASTER',
+        TURN_PLAYER: 'TURN_PLAYER',
+        LOSE_GAME_MASTER: 'LOSE_GAME_MASTER',
+        LOSE_PLAYER: 'LOSE_PLAYER'
+    };
+}());
