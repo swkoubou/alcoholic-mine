39cfcbf8d72926dfe3556a4c6f447a564af0af09
snakazawa <nakazawa.tms0314@gmail.com>
2016-08-07 15:34:38 +0900

    ゲーム設定のバリデーション実装

--- www/js/routes/settings.js
+++ www/js/routes/settings.js
@@ -4,10 +4,25 @@
             $$('.back-title-link').addClass('visible');
 
             $$(page.container).find('.submit-button').on('click', () => {
-                data.settings = f7App.formToJSON('#settings-form');
-                mainView.router.back();
+                const settings = f7App.formToJSON('#settings-form');
+
+                if (validateSettings(settings)) {
+                    data.settings = settings;
+                    mainView.router.back();
+                } else {
+                    f7App.alert('適切な値を入力してください。', '値が間違っています。');
+                }
             });
         };
     };
+
+    function validateSettings(settings) {
+        if (settings.users.split(',').length < 2) return false;
+        if (!_.inRange(settings.panelRow, 1, 6)) return false;
+        if (!_.inRange(settings.panelColumn, 1, 6)) return false;
+        if (!_.inRange(settings.colorNumber, 1, 6)) return false;
+        if (!_.inRange(settings.memorizeDuration, 500, 20001)) return false;
+        return true;
+    }
 
 }(alcoholicmine.routes, alcoholicmine.models, alcoholicmine.viewmodels, alcoholicmine.data, Dom7));
